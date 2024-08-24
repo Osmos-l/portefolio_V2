970490691426f7f24da5923a32ee7dce418577a0
Jordan LECLERCQ <emailjeux81@gmail.com>
2024-08-24 12:50:16 +0200

    feat: navbar website name

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
         <div className="w-full">
             <div className="mx-auto w-full max-w-7xl flex items-center justify-between py-4 px-12">
-                <p className="text-white">jordan.dev</p>
+                <a className="text-white" href="https://jordanlcq.vercel.app/">jordanlcq.vercel</a>
                 <p className="text-white">Contact me</p>
             </div>
             <div className="w-full h-[1px] from-transparent via-white to-transparent bg-gradient-to-r opacity-[.25]"></div>
