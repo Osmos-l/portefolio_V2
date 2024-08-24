7410726c98104698f8d7c26bd0d907a06b0d0a2a
Jordan LECLERCQ <emailjeux81@gmail.com>
2024-08-24 22:01:19 +0200

    i hope the end of experiences section

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -9,12 +9,12 @@ export type Experience = {
 
 const experiences: Experience[] = [
     {
-        company: "Stedis (2019)",
-        position: "Web developer",
-        website: "https://www.stedis.com/",
-        image: "/stedis.png",
-        skills: ["Java", "Spring", "JavaScript", "Angular"],
-        color: "green-700"
+        company: "CGI (2021 - today)",
+        position: "Software Engineer",
+        website: "https://www.cgi.com",
+        image: "/cgi.jpg",
+        skills: ["Ruby", "Rails", "JavaScript", "SQL", "Docker", "Kubernetes", "Jenkins"],
+        color: "pink-700"
     }, {
         company: "Sagesse Assurance (2020 - 2021)",
         position: "Software Engineer",
@@ -23,13 +23,13 @@ const experiences: Experience[] = [
         skills: ["PHP", "Symfony", "JavaScript", "SQL"],
         color: "orange-700"
     }, {
-        company: "CGI (2021 - today)",
-        position: "Software Engineer",
-        website: "https://www.cgi.com",
-        image: "/cgi.jpg",
-        skills: ["Ruby", "Rails", "JavaScript", "SQL", "Docker", "Kubernetes", "Jenkins"],
-        color: "pink-700"
-    }
+        company: "Stedis (2019)",
+        position: "Web developer",
+        website: "https://www.stedis.com/",
+        image: "/stedis.png",
+        skills: ["Java", "Spring", "JavaScript", "Angular"],
+        color: "green-700"
+    }, 
 ]
 
 export { experiences }
