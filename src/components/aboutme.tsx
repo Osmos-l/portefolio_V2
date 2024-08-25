cd6252ccc4f1a2d680820d0b67d36e7104119cd5
Jordan LECLERCQ <emailjeux81@gmail.com>
2024-08-25 18:10:56 +0200

    removing contact duplicated

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -11,24 +11,7 @@ export default function AboutMe() {
                     <br />
                     💡 I enjoy tackling complex technical challenges and creating exceptional user experiences. 
                     My goal is to continue pushing the boundaries of technology and contributing to impactful projects.
-                    <br />
-                    <br />
-                    Feel free to reach out to discuss new opportunities 😊.
                 </p>
-            </div>
-            <div className="flex items-center justify-center gap-6 py-3">
-                <a href="https://www.linkedin.com/in/jordanlcq" 
-                    className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md 
-                                hover:bg-blue-700 
-                                focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
-                    Linkedin
-                </a>
-                <a href="mailto:jordan.lcq1@gmail.com" 
-                    className="px-4 py-2 bg-gray-600 text-white font-semibold rounded-lg shadow-md 
-                                hover:bg-gray-700 
-                                focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75">
-                    Email
-                </a>
             </div>
         </div>
     )
